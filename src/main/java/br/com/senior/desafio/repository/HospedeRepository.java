d93acbb10a820ef43af46c010c50eb4220114032
jcafure <cafure.jaime@gmail.com>
2020-01-14 23:26:51 -0400

    first commit

--- src/main/java/br/com/senior/desafio/repository/HospedeRepository.java
+++ src/main/java/br/com/senior/desafio/repository/HospedeRepository.java
@@ -1,10 +1,10 @@
 package br.com.senior.desafio.repository;
 
 import br.com.senior.desafio.model.Hospede;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface HospedeRepository extends JpaRepository<Hospede, Integer> {
+public interface HospedeRepository extends PagingAndSortingRepository<Hospede, Integer> {
 
 }
