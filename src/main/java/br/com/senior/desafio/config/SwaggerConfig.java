2ca56f88eb5956f2a395fa685553f8d8b750bbdb
jcafure <cafure.jaime@gmail.com>
2020-01-18 10:38:39 -0400

    Add swagger

---
+++ src/main/java/br/com/senior/desafio/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package br.com.senior.desafio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.senior.desafio.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+
+        return new ApiInfo("API - Depósito de Bebidas",
+                "Api para controle de estoque de bebidas.",
+                "Documentação v1.0",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList());
+
+    }
+}
